3734a642a63b2c2361ef856007866f5ea2c5183a
Ejez <ejez@protonmail.ch>
2020-02-02 16:27:05 +0300

    chore: Update @quasar/qmarkdown and use toc-start

--- src/md/md-mixin.js
+++ src/md/md-mixin.js
@@ -12,17 +12,9 @@ export default {
       // initially 'this.$refs.markdown' is 'undefined', so we make sure it gets
       // defined before calling its function 'makeTree' which builds a
       // hierarchical tree of the table of contents
-      const tree = this.$refs.markdown
+      return this.$refs.markdown
         ? this.$refs.markdown.makeTree(this.tocData)
         : this.tocData
-
-      // usually the first item in the tree is the article title written in 'h1'
-      // heading, in most cases we don't want to display the article title, so
-      // to start from another level we normally use property 'toc-start',
-      // however due to a bug using 'toc-start' property, we do it manually here
-      return tree.length && tree[0].children.length
-        ? tree[0].children
-        : tree
     }
   },
 
